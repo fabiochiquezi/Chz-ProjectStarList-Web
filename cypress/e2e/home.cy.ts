07ce02aebc8ff8e3a52851135544f3f4aa7db0d4
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-09-09 08:08:24 -0300

    up cypress

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,9 @@
+describe('Home', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
+
+    it('contain all data', () => {
+        const h1 = cy.get('h1')
+    })
+})
