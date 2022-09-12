064453eacabb328651a059e183500e227c187bbc
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-09-12 12:17:13 -0300

    cypress config update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,20 @@
+import admin from 'firebase-admin'
 import { defineConfig } from 'cypress'
+import { myID } from 'firebase.mysettings'
+import { plugin as cypressFirebasePlugin } from 'cypress-firebase'
 
 export default defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
             // implement node event listeners here
+            cypressFirebasePlugin(on, config, admin)
         },
+        supportFile: 'cypress/support/e2e.ts',
         baseUrl: 'http://localhost:3000/',
         viewportHeight: 1080,
-        viewportWidth: 1920
+        viewportWidth: 1920,
+        env: {
+            TEST_UID: myID
+        }
     }
 })
---
+++ firebase.mysettings.example.ts
@@ -0,0 +1,4 @@
+// Put the ID of your GoogleLogin from your app
+// Enter at firebase console, access auth, find your google user, get from there the ID
+// This ID will be used on cypress to access the database and execute E2E tests
+export const myID = ''
