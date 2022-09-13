82e8bc31406edb5cb011c8af5cb78d6f9e245ea4
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-09-13 03:47:34 -0300

    jest/react-library config update

--- jest.config.ts
+++ jest.config.ts
@@ -67,7 +67,9 @@ export default {
     // A set of global variables that need to be available in all test environments
     // globals: {},
 
-    // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+    // The maximum amount of workers used to run your tests.
+    // Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number.
+    // maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
 
     // An array of directory names to be searched recursively up from the requiring module's location
@@ -142,7 +144,7 @@ export default {
     // setupFiles: [],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
-    setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 
     // The number of seconds after which a test is considered as slow and reported as such in the results.
     // slowTestThreshold: 5,
@@ -166,9 +168,11 @@ export default {
     // ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
-    // ],
+    testPathIgnorePatterns: [
+        '\\\\node_modules\\\\',
+        '<rootDir>/dist',
+        '<rootDir>/cypress'
+    ],
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
     // testRegex: [],
