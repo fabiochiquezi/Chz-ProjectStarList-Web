93dc2d8baeff0accc9312d8971d729eb49d891ac
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-09-13 05:24:45 -0300

    Buttons signIn

--- components/Buttons/SignIn/index.test.jsx
+++ components/Buttons/SignIn/index.test.jsx
@@ -1,25 +1,24 @@
-import { SignIn01 } from './index'
-const { render, fireEvent, screen } = require('@testing-library/react')
+import { SignInButton01, SignInButton02 } from './index'
+const { render, fireEvent } = require('@testing-library/react')
 
-describe.only('ButtonLoad', () => {
+describe.only('SignIn', () => {
     const callBack = jest.fn()
+    const Elem = <SignInButton01 onClick={callBack} />
+    const Elem2 = <SignInButton02 onClick={callBack} />
 
-
-    const Elem = <SignIn01 onClick={callBack} />
-    let el
-
-    beforeEach(() => {
+    it('should exist and have click event - btn1', () => {
         const { getByTestId } = render(Elem)
-        el = getByTestId('btn-signIn01')
+        const el1 = getByTestId('btn-signIn01')
+        expect(el1).toBeInTheDocument()
+        fireEvent.click(el1)
+        expect(callBack).toHaveBeenCalledTimes(1)
     })
 
-    it('exist', () => {
-        expect(el).toBeInTheDocument()
-    })
-
-    it('click event', () => {
-        fireEvent.click(el)
-        fireEvent.click(el)
-        expect(callBack).toHaveBeenCalledTimes(2)
+    it('should exist and have click event - btn2', () => {
+        const { getByTestId } = render(Elem2)
+        const el2 = getByTestId('btn-signIn02')
+        expect(el2).toBeInTheDocument()
+        fireEvent.click(el2)
+        expect(callBack).toHaveBeenCalledTimes(1)
     })
 })
--- components/Buttons/SignIn/index.tsx
+++ components/Buttons/SignIn/index.tsx
@@ -5,12 +5,29 @@ import { useAuth } from '../../../context/AuthContext/types'
 
 interface props {
     className?: string
-    onClick: () => Promise<void>
+    onClick: () => any
     id?: string
 }
 
-const SignIn01: React.FC<props> = ({ className = '', onClick, id = '' }) => {
+const SignInButton01: React.FC<props> = ({
+    className = '',
+    onClick,
+    id = ''
+}) => {
     const { loading } = useAuth()
+
+    const BtnLoad = (
+        <div className="mt-[7px]">
+            <SpinIcon2 />
+        </div>
+    )
+    const BtnIcon = (
+        <>
+            <span className="md:text-sm lg:text-md">Sign In</span>
+            <GoogleIcon width={20} height={20} className="ml-1 mt-[-3px]" />
+        </>
+    )
+    const Button = (): React.ReactElement => (loading ? BtnLoad : BtnIcon)
 
     return (
         <a
@@ -20,22 +37,41 @@ const SignIn01: React.FC<props> = ({ className = '', onClick, id = '' }) => {
             data-cy="btn-signIn1"
             data-testid="btn-signIn01"
         >
-            {loading ? (
-                <div className="mt-[7px]">
-                    <SpinIcon2 />
-                </div>
-            ) : (
-                <>
-                    <span className="md:text-sm lg:text-md">Sign In</span>
-                    <GoogleIcon
-                        width={20}
-                        height={20}
-                        className="ml-1 mt-[-3px]"
-                    />
-                </>
-            )}
+            <Button />
         </a>
     )
 }
 
-export { SignIn01 }
+const SignInButton02: React.FC<props> = ({
+    className = '',
+    onClick,
+    id = ''
+}) => {
+    const { loading } = useAuth()
+    const BtnLoad = (
+        <div className="mt-3">
+            <SpinIcon2 width={20} height={20} color="#FB923C" />
+        </div>
+    )
+    const BtnIcon = (
+        <>
+            <span>Sign In</span>
+            <GoogleIcon className="ml-2 mt-[-1px]" />
+        </>
+    )
+    const Button = (): React.ReactElement => (loading ? BtnLoad : BtnIcon)
+
+    return (
+        <button
+            className={`btn-transparent py-[12px] px-8 border-orange-400 text-orange-400 flex justify-center items-center text-lg h-[56px] w-[166px] ${className}`}
+            onClick={onClick}
+            id={id}
+            data-cy="btn-signIn2"
+            data-testid="btn-signIn02"
+        >
+            <Button />
+        </button>
+    )
+}
+
+export { SignInButton01, SignInButton02 }
