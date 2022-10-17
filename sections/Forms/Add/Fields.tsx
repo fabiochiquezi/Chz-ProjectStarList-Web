5d1a65ead71353548096fe8d89a81d50e27caf82
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-10-17 05:16:36 -0300

    add a description on box add

--- sections/Forms/Add/Fields.tsx
+++ sections/Forms/Add/Fields.tsx
@@ -15,9 +15,13 @@ const Fields: React.FC<Props> = ({ formik, loading, btnRef }) => {
 
     return (
         <>
-            <h3 className="mb-5 text-xl text-white font-bold mt-1">
+            <h3 className="mb-2 text-xl text-white font-bold mt-1">
                 Add new item
             </h3>
+            <p className="mb-5 text-white">
+                Search for the work you want on google, select images, choose
+                one, select &quot;copy link&quot; and paste here.
+            </p>
 
             <div className="mt-3 md:grid grid-cols-2 gap-x-8">
                 <Input
