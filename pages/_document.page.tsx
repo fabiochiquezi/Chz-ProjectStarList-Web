88caa45b8ba058ab5ee6314e09c0abbdffa12430
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-12-16 11:40:19 -0300

    set up styled-components

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -1,16 +1,57 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import React, { ReactElement } from 'react'
 
-export default function Document(): ReactElement {
-  return (
-    <Html className="dark" lang="en-US">
-      <Head>
-        <link rel="icon" type="image/x-icon" href="/favicon.png" />
-      </Head>
-      <body className="bg-primary">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import { ServerStyleSheet } from 'styled-components'
+import { ReactElement } from 'react'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<{
+    styles: ReactElement;
+    html: string;
+    head?: Array<ReactElement | null> | undefined;
+  }> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render(): ReactElement {
+    return (
+      <Html className="dark" lang="en-US">
+        <Head>
+          <title>Star List</title>
+          <link rel="icon" type="image/x-icon" href="/favicon.png" />
+        </Head>
+        <body className="bg-primary">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
