576b86074a18ffada3e68b88c86313a8198a0f4b
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-12-20 11:36:38 -0300

    test: test

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -7,7 +7,6 @@ import GlobalStyles from './GlobalStyles'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  console.log('ok')
   return (
     <>
       <Head>
